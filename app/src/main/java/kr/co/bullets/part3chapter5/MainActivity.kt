c3903b2a79dcfd03ef727725614aae893d2eb2e8
Bullets-jyr <jyr@bullets.co.kr>
2023-07-22 20:32:10 +0900

    02. 검색 UI 그리기 1

--- app/src/main/java/kr/co/bullets/part3chapter5/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part3chapter5/MainActivity.kt
@@ -2,10 +2,36 @@ package kr.co.bullets.part3chapter5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
+
+import kr.co.bullets.part3chapter5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            view = this@MainActivity
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return false
+                }
+            })
+        }
+        return true
     }
 }
