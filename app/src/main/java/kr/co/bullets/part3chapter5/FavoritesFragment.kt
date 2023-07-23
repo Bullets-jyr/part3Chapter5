ae91885e03d7e506675bc343104009af93b4a691
Bullets-jyr <jyr@bullets.co.kr>
2023-07-23 15:21:18 +0900

    05. 검색 UI 그리기 4

--- app/src/main/java/kr/co/bullets/part3chapter5/FavoritesFragment.kt
+++ app/src/main/java/kr/co/bullets/part3chapter5/FavoritesFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.co.bullets.part3chapter5.databinding.FragmentFavoritesBinding
+import kr.co.bullets.part3chapter5.list.ListAdapter
 
 class FavoritesFragment : Fragment() {
     private var binding: FragmentFavoritesBinding? = null
+    private val adapter by lazy {
+        ListAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +22,13 @@ class FavoritesFragment : Fragment() {
         return FragmentFavoritesBinding.inflate(inflater, container, false).apply {
             binding = this
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            recyclerView.adapter = adapter
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/kr/co/bullets/part3chapter5/SearchFragment.kt
+++ app/src/main/java/kr/co/bullets/part3chapter5/SearchFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.co.bullets.part3chapter5.databinding.FragmentSearchBinding
+import kr.co.bullets.part3chapter5.list.ListAdapter
 
 class SearchFragment : Fragment() {
     private var binding: FragmentSearchBinding? = null
+    private val adapter by lazy {
+        ListAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +22,13 @@ class SearchFragment : Fragment() {
         return FragmentSearchBinding.inflate(inflater, container, false).apply {
             binding = this
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            recyclerView.adapter = adapter
+        }
     }
 
     override fun onDestroyView() {
